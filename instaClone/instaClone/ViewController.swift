8f1fbc3c360ed37548e7f6e8fbe792002f0217b0
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-12-20 22:05:56 +0900

    폴더추가

---
+++ instaClone/instaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaClone
+//
+//  Created by 김혜란 on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
