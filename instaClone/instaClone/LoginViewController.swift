754c33e02fee5a643cfd4bc827377e3e77da4206
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-12-21 14:29:57 +0900

    로그인 파일 생성

---
+++ instaClone/instaClone/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  instaClone
+//
+//  Created by 김혜란 on 12/21/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    //이메일에 뭔가 입력될때마다 이 함수가 호출됨
+    @IBAction func emailTextFieldeditingChanged(_ sender: Any) {
+        print("호출됨")
+    }
+    
+
+}
