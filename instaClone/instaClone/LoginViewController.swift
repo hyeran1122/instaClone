9cd2f98ebe440bfe59feaa9d478b8be7758bcf42
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-12-21 16:02:22 +0900

    화면가입 화면

--- instaClone/instaClone/LoginViewController.swift
+++ instaClone/instaClone/LoginViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
 
+class LoginViewController: UIViewController {
+    
+    var eamil = String()
+    var password = String()
     
     
     
@@ -18,7 +21,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
         
         
@@ -29,9 +32,29 @@ class LoginViewController: UIViewController {
     
     
     //이메일에 뭔가 입력될때마다 이 함수가 호출됨
-    @IBAction func emailTextFieldeditingChanged(_ sender: Any) {
-        print("호출됨")
+    @IBAction func emailTextFieldEditingChange(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        print(text)
     }
     
-
+    @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
+        
+    }
+    
+    
+    @IBAction func loginButtonDidTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func registerButtonDidTap(_ sender: UIButton) {
+        //1. 스토리 보드 생성
+        let stotyboard = UIStoryboard(name:"Main", bundle: nil)
+        
+        //2. 뷰컨트롤러 생성
+        let registerViewControllor = stotyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
+        
+        //3. 화면전환
+        self.navigationController?.pushViewController(registerViewControllor, animated: true)
+    }
+    
 }
