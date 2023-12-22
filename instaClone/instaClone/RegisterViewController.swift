4396199f32c0c20f57bc9a9e62f133f313e85823
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-12-22 23:58:31 +0900

    화면가입 액션연결

--- instaClone/instaClone/RegisterViewController.swift
+++ instaClone/instaClone/RegisterViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    //MARK: - Ptoperties
+    var isValidEmail = false
+    var isvalidName = false
+    var isValidNickName = false
+    var isValidPassword = false
+    
+    //텍스트 필드가져오기
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nickNameTextField: UITextField!
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var textFields: [UITextField] {
+        [emailTextField, nameTextField, nickNameTextField, passwordTextField]
+    }
+    
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    //MARK: - Acitons
+    @objc
+    func textFieldEditingChange(_ sender: UITextField) {
+        
+        
     }
-    */
+    
+    
+    
+    //MARK: - Helpers
 
+    //연결 메소드
+    private func setupTextField() {
+        
+//        emailTextField.addTarget(self, action: #selector(textFieldEditingChange(_:)), for: .editingChanged)
+    }
+    
+    
+    
 }
