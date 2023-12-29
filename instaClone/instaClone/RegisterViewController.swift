77e843ca57d056b39f4a683075692581d40b8246
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-12-29 16:11:00 +0900

    유효성 검사 적용

--- instaClone/instaClone/RegisterViewController.swift
+++ instaClone/instaClone/RegisterViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     //MARK: - Ptoperties
-    var isValidEmail = false
-    var isvalidName = false
-    var isValidNickName = false
-    var isValidPassword = false
+    var isValidEmail = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
+    
+    var isvalidName = false{
+        didSet {
+            self.validateUserInfo()
+        }
+    }
+    
+    var isValidNickName = false{
+        didSet {
+            self.validateUserInfo()
+        }
+    }
+    
+    var isValidPassword = false{
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
     //텍스트 필드가져오기
     
@@ -24,6 +43,10 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    
+    @IBOutlet weak var singupButton: UIButton!
+    
+    
     var textFields: [UITextField] {
         [emailTextField, nameTextField, nickNameTextField, passwordTextField]
     }
@@ -32,7 +55,7 @@ class RegisterViewController: UIViewController {
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-         
+        
     }
     
     
@@ -44,13 +67,17 @@ class RegisterViewController: UIViewController {
         
         switch sender {
         case emailTextField:
-            print("email")
+            self.isValidEmail = text.isValidEmail()
+            
         case nameTextField:
-            print("name")
+            self.isvalidName = text.count > 2
+            
         case nickNameTextField:
-            print("nickname")
+            self.isValidNickName = text.count > 2
+            
         case passwordTextField:
-            print("password")
+            self.isValidPassword = text.isValidPassword()
+            
         default:
             fatalError("Missing")
             
@@ -72,5 +99,39 @@ class RegisterViewController: UIViewController {
     }
     
     
-    
+    private func validateUserInfo() {
+        
+        if isValidEmail
+            && isvalidName
+            && isValidNickName
+            && isValidPassword {
+            
+            UIView.animate(withDuration: 0.33) {
+                self.singupButton.backgroundColor = UIColor.facebool
+            }
+        } else {
+            UIView.animate(withDuration: 0.33) {
+                self.singupButton.isEnabled = false
+                self.singupButton.backgroundColor = .disabledButton
+            }
+        }
+    }
 }
+
+
+//정규 표현식
+extension String {
+    func isValidPassword() -> Bool {
+        let regularExpression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&](8,]"
+        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
+        
+        return passwordValidation.evaluate(with: self)
+    }
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]=@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
+
