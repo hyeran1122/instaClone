2820f77ed054736f70b5997f69e62d1dbe1d32f9
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-01-04 22:41:54 +0900

    instaClone 커밋

--- instaClone/LoginViewController.swift
+++ instaClone/LoginViewController.swift
@@ -51,7 +51,9 @@ class LoginViewController: UIViewController {
         
         if userInfo.email == self.email
             && userInfo.password == self.password {
-            let vc = storyboard?.instantiateViewController(withIdentifier: "TextVC") as! testViewController; self.present(vc, animated: true, completion: nil)
+            let vc = storyboard?.instantiateViewController(withIdentifier: "TextVC") as! UITabBarController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
         }
         else {
             
